1bab005ef5f58fb0c792690c74ba0f376e8a68a1
Dominika <dominikapiganova@mail.ru>
2022-02-22 16:23:51 +0200

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Dominika Poleshyck on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
