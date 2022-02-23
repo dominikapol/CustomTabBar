001967f7b9f97cfceab8473e471c8343e71711dc
Dominika <dominikapiganova@mail.ru>
2022-02-23 10:59:55 +0200

    Custom TabBar with SwiftUI

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selected = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            ZStack {
+                HStack {
+                    Button(action: { self.selected = 0 }) {
+                        Image("hotdog")
+                    } .foregroundColor(self.selected == 0 ? .black : .white)
+                    Spacer(minLength: 12)
+                    
+                    Button(action: { self.selected = 1 }) {
+                        Image("taco")
+                    } .foregroundColor(self.selected == 1 ? .black : .white)
+                    Spacer().frame(width: 120)
+                    
+                    Button(action: { self.selected = 2 }) {
+                        Image("icecream")
+                    } .foregroundColor(self.selected == 2 ? .black : .white)
+                    Spacer(minLength: 12)
+                    
+                    Button(action: { self.selected = 2 }) {
+                        Image("pizza")
+                    } .foregroundColor(self.selected == 2 ? .black : .white)
+                    
+                } .padding()
+                    .padding(.horizontal, 0)
+                    .background(CurvedShape())
+                Button {
+                    //
+                } label: {
+                    Image("heart")
+                        .renderingMode(.original)
+                        .padding(18)
+                } .background(.yellow)
+                    .clipShape(Circle())
+                    .offset(y: -32)
+                    .shadow(radius: 5)
+
+            }
+        } .background(.orange)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -19,3 +58,17 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CurvedShape: View  {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 70))
+            path.addArc(center: CGPoint(x: UIScreen.main.bounds.width / 2, y: 70), radius: 50, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: true)
+            path.addLine(to: CGPoint(x: 0, y: 70))
+        } .fill(Color.indigo)
+            .rotationEffect(.init(degrees: 180))
+    }
+}
+
